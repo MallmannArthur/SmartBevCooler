5a984625fca24826f34f386394f860661ea06928
MallmannArthur <arthurhenriquemallmann@gmail.com>
2022-10-24 12:01:42 -0300

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,25 @@
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
- #define SENSOR_LEVEL10 12 //pino ligado ao sensor é 18 e GND
- #define SENSOR_LEVEL50 14 //pino ligado ao sensor é 19 e GND
+#define SENSOR_LEVEL10 12 //pino ligado ao sensor é D4 e GND
+#define SENSOR_LEVEL50 14 //pino ligado ao sensor é D5 e GND
+#define ONE_WIRE_BUS 4//pino ligado ao atuador é D2
+#define RELAY 2
  
- bool flag_nivel10_baixo = false;
- bool flag_nivel10_acima = false;
- bool flag_nivel50_baixo = false;
- bool flag_nivel50_acima = false;
- unsigned long time_nivel10 = 0;
- unsigned long lasttime_nivel10 = 0;
- unsigned long time_nivel50 = 0;
- unsigned long lasttime_nivel50 = 0;
 
-const int oneWireBus = 4;
-OneWire oneWire(oneWireBus);
+// Setting up Global variables
+bool flag_nivel10_baixo = false;
+bool flag_nivel10_acima = false;
+bool flag_nivel50_baixo = false;
+bool flag_nivel50_acima = false;
+unsigned long time_nivel10 = 0;
+unsigned long lasttime_nivel10 = 0;
+unsigned long time_nivel50 = 0;
+unsigned long lasttime_nivel50 = 0;
+OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "arthur";
+char ssid[] = "arthur"; 
 char pass[] = "arthur01";
 BlynkTimer timer;
 WidgetLED led50(V5);
@@ -53,7 +55,13 @@ void myTimerEvent()
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
   sensors.requestTemperatures();
-  float t = sensors.getTempCByIndex(0); 
+  float t = sensors.getTempCByIndex(0);
+  if (t <= 24) {
+    digitalWrite(RELAY, LOW);
+  } 
+  if (t>= 25 ){
+    digitalWrite(RELAY, HIGH);
+  }
   Blynk.virtualWrite(V4, t);
 }
 
@@ -128,79 +136,3 @@ void loop()
   timer.run();
   checkLevelSensors();
 }
-
-
-
-/*
-//esp
-#include <Arduino.h>
- #define SENSOR_LEVEL10 12 //pino ligado ao sensor é 18 e GND
- #define SENSOR_LEVEL50 14 //pino ligado ao sensor é 19 e GND
- 
- bool flag_nivel10_baixo = false;
- bool flag_nivel10_acima = false;
- bool flag_nivel50_baixo = false;
- bool flag_nivel50_acima = false;
- unsigned long time_nivel10 = 0;
- unsigned long lasttime_nivel10 = 0;
- unsigned long time_nivel50 = 0;
- unsigned long lasttime_nivel50 = 0;
- 
- void IRAM_ATTR level10ISR() { //interrupt para detectar mudança, apenas levanta flag, prints e dalays fora para não causar wdt timeout
-  time_nivel10 = millis(); //milis retorna tempo de execução atual reseta em ~50d
-  if (time_nivel10 - lasttime_nivel10 > 100) {
-    if (digitalRead(SENSOR_LEVEL10) == HIGH) {
-      flag_nivel10_baixo = true;
-    } else {
-    flag_nivel10_acima = true;
-  }
-  lasttime_nivel10 = time_nivel10;
-  }
- }
-
- void IRAM_ATTR level50ISR() {
- time_nivel50 = millis();
- if (time_nivel50 - lasttime_nivel50 > 100) {
- if (digitalRead(SENSOR_LEVEL50) == HIGH) {
- flag_nivel50_baixo = true;
- } else {
- flag_nivel50_acima = true;
- }
- lasttime_nivel50 = time_nivel50;
- }
- }
-
- void setupLevelSensors() {
- pinMode(SENSOR_LEVEL10, INPUT_PULLUP);
- attachInterrupt(SENSOR_LEVEL10, level10ISR, CHANGE);
- pinMode(SENSOR_LEVEL50, INPUT_PULLUP);
- attachInterrupt(SENSOR_LEVEL50, level50ISR, CHANGE);
- }
-
- void checkLevelSensors() {
- if (flag_nivel10_baixo == true) {
- flag_nivel10_baixo = false;
- Serial.println("abaixo 10");
- }
- if (flag_nivel10_acima == true) {
- flag_nivel10_acima = false;
- Serial.println("acima 10");
- }
- if (flag_nivel50_baixo == true) {
- flag_nivel50_baixo = false;
- Serial.println("abaixo 50");
- }
- if (flag_nivel50_acima == true) {
- flag_nivel50_acima = false;
- Serial.println("acima 50");
- }
- }
-
- void setup() {
- Serial.begin(9600);
- setupLevelSensors();
- }
-
-void loop() {
- checkLevelSensors();
- }*/
